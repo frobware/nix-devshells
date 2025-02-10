f1a704b7d87dff052fb19a30499eeaf7d75452e5
Andrew McDermott <aim@frobware.com>
2025-02-10 10:30:28 +0000

    rust/default: sort and format

--- rust/default.nix
+++ rust/default.nix
@@ -1,47 +1,43 @@
 { pkgs, rustVersion }:
 
 let
-  # Select the appropriate Rust toolchain version
   toolchain = if rustVersion == "nightly" then
-  pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default)
+    pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default)
   else
-  pkgs.rust-bin.${rustVersion}.latest.default;
+    pkgs.rust-bin.${rustVersion}.latest.default;
 in pkgs.mkShell {
   buildInputs = [
     toolchain
     pkgs.clang
     pkgs.cmake
     pkgs.llvmPackages.libclang
+    pkgs.llvmPackages_latest.lldb
     pkgs.mold
     pkgs.ninja
-    pkgs.pkg-config
-    pkgs.llvmPackages_latest.lldb
-    pkgs.rust-analyzer
     pkgs.openssl
     pkgs.openssl.dev
+    pkgs.pkg-config
+    pkgs.rust-analyzer
     pkgs.sqlite
   ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
     pkgs.darwin.apple_sdk.frameworks.Security
     pkgs.darwin.apple_sdk.frameworks.CoreFoundation
     pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-  ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
-    pkgs.gdb
-    pkgs.valgrind
-  ];
+  ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ pkgs.gdb pkgs.valgrind ];
 
   env = {
     LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
     RUST_BACKTRACE = "1";
     RUSTFLAGS = if pkgs.stdenv.isDarwin then
-    "-C link-arg=-fuse-ld=/usr/bin/ld"
+      "-C link-arg=-fuse-ld=/usr/bin/ld"
     else
-    "-C link-arg=-fuse-ld=mold";
+      "-C link-arg=-fuse-ld=mold";
   };
 
   shellHook = ''
     export CARGO_TARGET_DIR="/tmp/cargo-target-dir-''${USER:-unknown-user}-$(basename "$PWD")"
-    mkdir -p "''$CARGO_TARGET_DIR"
+    mkdir -p "$CARGO_TARGET_DIR"
     ln -sf "$CARGO_TARGET_DIR" target
     echo CARGO_TARGET_DIR=$CARGO_TARGET_DIR
     echo "🦀🦀🦀 Welcome to your Rust development shell (${rustVersion}) 🦀🦀🦀"
